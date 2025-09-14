349faa0d478ba07e53695b620f1df14082e9731c
Sawom <asawom250@gmail.com>
2025-09-15 00:52:34 +0600

    auth service, axios instance

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,5 +1,6 @@
 import { authKey } from "@/constants/authkey";
 import { decodedToken } from "@/utils/jwt";
+import { instance as axiosInstance } from "@/helpers/axios/axiosInstance";
 
 import {
   getFromLocalStorage,
@@ -33,4 +34,13 @@ export const isLoggedIn = () => {
 
 export const removeUser = () => {
   return removeFromLocalStorage(authKey);
+};
+
+export const getNewAccessToken = async () => {
+  return await axiosInstance({
+    url: "http://localhost:5000/api/v1/auth/refresh-token",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    withCredentials: true,
+  });
 };
