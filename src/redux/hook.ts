5c673f5c017e7ced6c06e22df8158075cf2d5437
Sawom <asawom250@gmail.com>
2025-09-14 23:27:54 +0600

    forms components added

--- src/redux/hook.ts
+++ src/redux/hook.ts
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "./store";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
+
+type TDebouncedProps = {
+  searchQuery: string;
+  delay: number;
+};
+
+// when write something in the search box then it shows the result after a while of time
+// it will reduce the number of api calls. because when we write something in the search box
+// it will wait for the delay time and then it will show the result
+// if we write something again in the search box then it will wait for the delay time again
+// and then it will show the result
+// this is called debouncing
+export const useDebounced = ({ searchQuery, delay }: TDebouncedProps) => {
+  const [debouncedValue, setDebouncedValue] = useState<string>(searchQuery);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(searchQuery);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchQuery, delay]);
+
+  return debouncedValue;
+};
