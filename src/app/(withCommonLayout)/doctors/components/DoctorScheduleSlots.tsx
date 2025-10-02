8d0133697a2397a260f044308fc3f1b72a9b5332
Sawom <asawom250@gmail.com>
2025-10-03 01:54:51 +0600

    doctor schedule update and appointment api

--- src/app/(withCommonLayout)/doctors/components/DoctorScheduleSlots.tsx
+++ src/app/(withCommonLayout)/doctors/components/DoctorScheduleSlots.tsx
@@ -10,97 +10,202 @@ import { useState } from "react";
 dayjs.extend(utc);
 
 const DoctorScheduleSlots = ({ id }: { id: string }) => {
-  const [scheduleId, setScheduleId] = useState("");
+   const [scheduleId, setScheduleId] = useState('');
 
-  const query: Record<string, any> = {};
+   const router = useRouter();
 
-  query["doctorId"] = id;
+   const query: Record<string, any> = {};
 
-  query["startDate"] = dayjs(new Date())
-    .utc()
-    .hour(0)
-    .minute(0)
-    .second(0)
-    .millisecond(0)
-    .toISOString();
+   query['doctorId'] = id;
 
-  query["endDate"] = dayjs(new Date())
-    .utc()
-    .hour(23)
-    .minute(59)
-    .second(59)
-    .millisecond(999)
-    .toISOString();
+   query['startDate'] = dayjs(new Date())
+      .utc()
+      .hour(0)
+      .minute(0)
+      .second(0)
+      .millisecond(0)
+      .toISOString();
 
-  const { data, isLoading } = useGetAllDoctorSchedulesQuery({ ...query });
+   query['endDate'] = dayjs(new Date())
+      .utc()
+      .hour(23)
+      .minute(59)
+      .second(59)
+      .millisecond(999)
+      .toISOString();
 
-  const doctorSchedules = data?.doctorSchedules;
+   const { data, isLoading } = useGetAllDoctorSchedulesQuery({ ...query });
 
-  console.log(doctorSchedules);
+   const doctorSchedules = data?.doctorSchedules;
 
-  const currentDate = new Date();
-  const today = currentDate.toLocaleDateString("en-US", { weekday: "long" });
+   console.log(doctorSchedules);
 
-  const availableSlots = doctorSchedules?.filter(
-    (doctor: DoctorSchedule) => !doctor.isBooked
-  );
+   const currentDate = new Date();
+   const today = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
 
-  const handleBookAppointment = async () => {};
+   const nextDate = new Date(currentDate);
+   nextDate.setDate(currentDate.getDate() + 1);
+   const tomorrow = nextDate.toLocaleDateString('en-US', { weekday: 'long' });
 
-  return (
-    <Box mb={5}>
-      <Box sx={{ bgcolor: "white", p: 3, mt: 1 }}>
-        <Typography variant="h4" mb={3} color="primary.main">
-          Availability
-        </Typography>
-        <Typography variant="h6" fontSize={16}>
-          <b>Today: {dateFormatter(currentDate.toISOString()) + " " + today}</b>
-        </Typography>
-        <Box sx={{ borderBottom: "2px dashed #d0d0d0", mt: 2, mb: 3 }} />
-        <Stack direction="row" alignItems="center" flexWrap="wrap" gap={2}>
-          {availableSlots?.length ? (
-            isLoading ? (
-              "Loading..."
-            ) : (
-              availableSlots?.map((doctorSchedule: DoctorSchedule) => {
-                const formattedTimeSlot = `${getTimeIn12HourFormat(
-                  doctorSchedule?.schedule?.startDate
-                )} - ${getTimeIn12HourFormat(
-                  doctorSchedule?.schedule?.endDate
-                )}`;
+   // query params for next date
+   query.startDate = dayjs(nextDate)
+      .utc()
+      .hour(0)
+      .minute(0)
+      .second(0)
+      .millisecond(0)
+      .toISOString();
 
-                return (
-                  <Button
-                    key={doctorSchedule?.scheduleId}
-                    color="primary"
-                    onClick={() => setScheduleId(doctorSchedule?.scheduleId)}
-                    variant={`${
-                      doctorSchedule?.scheduleId === scheduleId
-                        ? "contained"
-                        : "outlined"
-                    }`}
-                  >
-                    {formattedTimeSlot}
-                  </Button>
-                );
-              })
-            )
-          ) : (
-            <span style={{ color: "red" }}>
-              No Schedule is Available Today!
-            </span>
-          )}
-        </Stack>
+   query.endDate = dayjs(nextDate)
+      .utc()
+      .hour(23)
+      .minute(59)
+      .second(59)
+      .millisecond(999)
+      .toISOString();
+
+   const { data: nextDoctorSchedules, isLoading: loading } =
+      useGetAllDoctorSchedulesQuery({
+         ...query,
+      });
+   const schedulesOfTomorrow = nextDoctorSchedules?.doctorSchedules;
+
+   const availableSlots = doctorSchedules?.filter(
+      (doctor: DoctorSchedule) => !doctor.isBooked
+   );
+
+   const availableNextDaySlots = schedulesOfTomorrow?.filter(
+      (doctor: DoctorSchedule) => !doctor.isBooked
+   );
+
+   const [createAppointment] = useCreateAppointmentMutation();
+   const [initialPayment] = useInitialPaymentMutation();
+
+   const handleBookAppointment = async () => {
+      try {
+         if (id && scheduleId) {
+            const res = await createAppointment({
+               doctorId: id,
+               scheduleId,
+            }).unwrap();
+
+            if (res.id) {
+               const response = await initialPayment(res.id).unwrap();
+
+               if (response.paymentUrl) {
+                  router.push(response.paymentUrl);
+               }
+            }
+         }
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
+   return (
+      <Box mb={5}>
+         <Box sx={{ bgcolor: 'white', p: 3, mt: 1 }}>
+            <Typography variant='h4' mb={3} color='primary.main'>
+               Availability
+            </Typography>
+            <Typography variant='h6' fontSize={16}>
+               <b>
+                  Today:{' '}
+                  {dateFormatter(currentDate.toISOString()) + ' ' + today}
+               </b>
+            </Typography>
+            <Box sx={{ borderBottom: '2px dashed #d0d0d0', mt: 2, mb: 3 }} />
+            <Stack direction='row' alignItems='center' flexWrap='wrap' gap={2}>
+               {availableSlots?.length ? (
+                  isLoading ? (
+                     'Loading...'
+                  ) : (
+                     availableSlots?.map((doctorSchedule: DoctorSchedule) => {
+                        const formattedTimeSlot = `${getTimeIn12HourFormat(
+                           doctorSchedule?.schedule?.startDate
+                        )} - ${getTimeIn12HourFormat(
+                           doctorSchedule?.schedule?.endDate
+                        )}`;
+
+                        return (
+                           <Button
+                              key={doctorSchedule?.scheduleId}
+                              color='primary'
+                              onClick={() =>
+                                 setScheduleId(doctorSchedule?.scheduleId)
+                              }
+                              variant={`${
+                                 doctorSchedule?.scheduleId === scheduleId
+                                    ? 'contained'
+                                    : 'outlined'
+                              }`}
+                           >
+                              {formattedTimeSlot}
+                           </Button>
+                        );
+                     })
+                  )
+               ) : (
+                  <span style={{ color: 'red' }}>
+                     No Schedule is Available Today!
+                  </span>
+               )}
+            </Stack>
+            <Typography variant='h6' fontSize={16} mt={5}>
+               <b>
+                  Tomorrow:{' '}
+                  {dateFormatter(nextDate.toISOString()) + ' ' + tomorrow}
+               </b>
+            </Typography>
+            <Box sx={{ borderBottom: '2px dashed #d0d0d0', mt: 2, mb: 3 }} />
+            <Stack direction='row' alignItems='center' flexWrap='wrap' gap={2}>
+               {availableNextDaySlots?.length ? (
+                  isLoading ? (
+                     'Loading...'
+                  ) : (
+                     availableNextDaySlots?.map(
+                        (doctorSchedule: DoctorSchedule) => {
+                           const formattedTimeSlot = `${getTimeIn12HourFormat(
+                              doctorSchedule?.schedule?.startDate
+                           )} - ${getTimeIn12HourFormat(
+                              doctorSchedule?.schedule?.endDate
+                           )}`;
+
+                           return (
+                              <Button
+                                 key={doctorSchedule?.scheduleId}
+                                 color='primary'
+                                 onClick={() =>
+                                    setScheduleId(doctorSchedule?.scheduleId)
+                                 }
+                                 variant={`${
+                                    doctorSchedule?.scheduleId === scheduleId
+                                       ? 'contained'
+                                       : 'outlined'
+                                 }`}
+                              >
+                                 {formattedTimeSlot}
+                              </Button>
+                           );
+                        }
+                     )
+                  )
+               ) : (
+                  <span style={{ color: 'red' }}>
+                     No Schedule is Available Today!
+                  </span>
+               )}
+            </Stack>
+         </Box>
+
+         <Button
+            onClick={handleBookAppointment}
+            sx={{ display: 'block', mx: 'auto' }}
+         >
+            Book Appointment Now
+         </Button>
       </Box>
-
-      <Button
-        onClick={handleBookAppointment}
-        sx={{ display: "block", mx: "auto" }}
-      >
-        Book Appointment Now
-      </Button>
-    </Box>
-  );
+   );
 };
 
 export default DoctorScheduleSlots;
